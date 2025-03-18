fb9bcdb4d96adb7c638bed4c25fbe3fde5233b6e
frida-holmer <frida.holmer@hotmail.com>
2025-03-18 17:06:03 +0100

    Adding search bar. Work in progress.

---
+++ src/interfaces/searchResultData.ts
@@ -0,0 +1,8 @@
+// interface for search result product page
+export interface SearchResultData {
+    id: number;
+    thumbnail: string;
+    title: string;
+    description: string;
+    price: number;
+}
