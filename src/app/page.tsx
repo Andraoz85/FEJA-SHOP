ee461e7a8c8dc1e1a49d877118735e118b2b0a86
zuekiin <elio.cadiz@gmail.com>
2025-03-19 10:51:31 +0100

    added shipping total functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { CartProvider } from "../context/CartContext";
+import type { AppProps } from "next/app";
 
-export default function Home() {
-
+export default function Home({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.page}>
-        <h1>Welcome to FEJA-Shop!</h1>
-        <p>A web-shop built with Next.js.</p>
+      <h1>Welcome to FEJA-Shop!</h1>
+      <p>A web-shop built with Next.js.</p>
     </div>
   );
 }
