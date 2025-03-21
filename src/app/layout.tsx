2601e14c2be4105b344d4e8da11d079e2fb9f84a
Andraoz85 <andraoz85@gmail.com>
2025-03-21 15:53:49 +0100

    implement search functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from "../components/Header";
+import Header from "@/components/Header";
+import Providers from "@/components/Providers";
 import "./globals.css";
-import { SearchProvider } from "@/context/searchContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,14 +28,14 @@ export default function RootLayout({
   return (
     <html lang="sv">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <SearchProvider>
+        <Providers>
           <Header />
           <div className="min-h-screen flex flex-col items-center">
             <div className="max-w-4xl w-full px-4">
               <main>{children}</main>
             </div>
           </div>
-        </SearchProvider>
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
+
 import { fetchProducts } from "@/actions/fetchData";
 import ProductList from "@/components/ProductList";
 import SearchResults from "@/components/SearchResults";
 import { useSearch } from "@/context/searchContext";
-import { Product } from "@/interfaces/Product";
 import { useEffect, useState } from "react";
+import { Product } from "@/interfaces/Product";
 
 export default function Home() {
   const { results } = useSearch();
-  console.log("Current search results:", results);
   const [products, setProducts] = useState<Product[]>([]);
-  
+
   useEffect(() => {
     const loadProducts = async () => {
       const data = await fetchProducts();
@@ -22,7 +22,6 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Products</h1>
-
       {results.length > 0 ? (
         <SearchResults />
       ) : (
