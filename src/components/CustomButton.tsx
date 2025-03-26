8bc4b23061b6389b3fd0391d2f44135d901ee589
frida-holmer <frida.holmer@hotmail.com>
2025-03-26 10:56:19 +0100

    Added styling for global button.

--- src/components/CustomButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, ButtonProps } from "@/components/ui/button"; // Testing
-import styles from "./button.module.css";
-import clsx from "clsx";
-
-export function CustomButton({ className, ...props }: ButtonProps) {
-  return <Button className={clsx(styles.customButton, className)} {...props} />;
-}
